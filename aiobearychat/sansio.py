c4da42717f33ec4fa4d19efdf2cf7ffdbd68adf2
mozillazg <mozillazg101@gmail.com>
2017-08-22 22:51:09 +0800

    删除变量类型注解，支持 Python 3.5
    改为使用注释来注解变量类型，同时解决 readthedocs 中无法正常生成文档的问题 #1

--- aiobearychat/sansio.py
+++ aiobearychat/sansio.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
-from typing import Dict, Optional
+from typing import Dict, Optional, Any
 from urllib.parse import urlencode
 
 import attr
@@ -8,21 +8,24 @@ import attr
 
 @attr.s
 class Response:
-    status: str = attr.ib()
-    headers: Dict = attr.ib()
-    body: bytes = attr.ib()
+    """请求响应信息"""
+    #: 状态码，比如 200
+    status = attr.ib()  # type: int
+    headers = attr.ib()  # type: Dict
+    body = attr.ib()  # type: bytes
 
-    def json(self):
+    def json(self) -> Any:
+        """反序列化 ``body`` 的值"""
         return json.loads(self.body)
 
 
-def format_url(url, url_params: Optional[Dict] = None):
+def format_url(url: str, url_params: Optional[Dict] = None) -> str:
     if not url_params:
         return url
     return url + '?' + urlencode(url_params)
 
 
-def clean_nothing_keys(origin_dict: Dict):
+def clean_nothing_keys(origin_dict: Dict) -> Dict:
     """移除值为 NOTHING 的 key"""
     return {
         k: v
